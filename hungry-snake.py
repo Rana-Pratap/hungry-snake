1ed31143e2a827d3d16983a16082c2b04ef4f96c
Rana Pratap Singh <108171374+Rana-Pratap@users.noreply.github.com>
2022-12-04 13:20:25 +0530

    Changed the programming paradigm to OOPs for better understanding of code.

--- hungry-snake.py
+++ hungry-snake.py
@@ -1,52 +1,62 @@
 import pygame
 from pygame.locals import *
-# import time
-
-def draw_block():
-    surface.fill((130, 244, 250))
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
-
-if __name__ == "__main__":
-    pygame.init()
-
-    # set the size of game window
-    surface = pygame.display.set_mode((1200, 675))
-
-    # pick color of our choise to fill game window from black
-    surface.fill((130, 244, 250))
-
-    block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 50
-    block_y = 50
-    surface.blit(block,(block_x, block_y))
-
-    # this will change black to our color of choice
-    pygame.display.flip()
 
 
-    running =True
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+class Snake():
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((130, 244, 250))
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
+    
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+    def move_right(self):
+        self.x += 10
+        self.draw()
+    def move_down(self):
+        self.y += 10
+        self.draw()
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1200, 675))
+        self.surface.fill((130, 244, 250))
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+    
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                    if event.key == K_UP:
+                        self.snake.move_up()
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+                elif event.type == QUIT:
                     running = False
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_block()
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_block()
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_block()
-            elif event.type == QUIT:
-                running = False
+
+    
+if __name__ == "__main__":
+    game = Game()
+    game.run()
 
 
-    # to keep the window for certain amount of time
-    # time.sleep(10)
- 
+    
